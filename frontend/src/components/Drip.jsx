577fa6298933e0f8e3bec3b345106551f291a3f6
unknown <ashmitgupta03@gmail.com>
2023-07-19 23:55:11 +0530

    text-editor-raw-code-added-modified-1

---
+++ frontend/src/components/Drip.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Box } from "@mui/icons-material"
+const Drip = () => {
+  return (
+    <>
+      <Box >
+
+      </Box>
+    </>
+  )
+}
+
+export default Drip
---
+++ frontend/src/components/MessageBox.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MessageBox = () => {
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default MessageBox
